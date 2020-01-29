97f8ddf597f93bfdb36e2aae06ab83df1a791ad2
ruru56 <60423917+ruru56@users.noreply.github.com>
2020-01-29 12:27:43 +0100

    Update monjs.js

--- monjs.js
+++ monjs.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	$('h1').hide();
+	//$('h1').hide(5000);
+
+	for (var i = 0; i < 5; i++) {
+		$( '<div class="col-1 boite">').appendTo( '#effets' );
+	}
+$("#effets div" ).click(function() {
+ $(this).hide( 2000, function() {
+	$(this).remove();
+	});
+});
+
+
 });
